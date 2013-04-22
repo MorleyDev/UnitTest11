9c39de6665d5c37cab1b947132439f899b3a5fdd
MorleyDev <jason@morleydev.co.uk>
2013-04-22 22:14:22 +0100

    Modified build scripts to end if cmake fails

--- Scripts/build.bat
+++ Scripts/build.bat
@@ -1,6 +1,7 @@
 mkdir ..\Build
 pushd ..\Build
 	cmake ..\ -G"MinGW Makefiles" -DCMAKE_MAKE_PROGRAM=make -DCMAKE_BUILD_TYPE=Release
+	IF NOT %ERRORLEVEL% EQU 0 EXIT %ERRORLEVEL%
 	make
 popd
 
--- Scripts/build.sh
+++ Scripts/build.sh
@@ -2,6 +2,12 @@
 
 pushd ../Build
 	cmake ../ -G"Unix Makefiles" -DCMAKE_MAKE_PROGRAM=make -DCMAKE_BUILD_TYPE=Release
+	
+	rc=$?
+	if [[ $rc != 0 ]] ; then
+		exit $rc
+	fi
+	
 	make
 popd
 
